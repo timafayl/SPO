86be6bc880362357679714aad6679505c12ca7bb
timafayl <timafayl@gmail.com>
2017-02-20 18:14:54 +0700

    Car

---
+++ FuelCalculation/FuelCalculation/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelCalculation
+{
+    public class Car : ITransportProperty
+    {
+        public double GetFuelWaste()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTankVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetWay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetWearRate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SetFuelWaste()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SetSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SetTankVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SetWay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SetWearRate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SuccessProbobility()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
