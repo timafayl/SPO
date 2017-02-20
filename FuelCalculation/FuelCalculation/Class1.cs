b50d0c5000f5b07af9013721906f2b949de66447
timafayl <timafayl@gmail.com>
2017-02-20 18:13:01 +0700

    класс Car

---
+++ FuelCalculation/FuelCalculation/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelCalculation
+{
+    public class Car: ITransport
+    {
+        private double _startFuelVolume;
+        private double _endFuelVolume;
+        private double _passedWay;
+        public double FuelCosts()
+        {
+            
+        }
+    }
+}
--- FuelCalculation/FuelCalculation/IFuelCalc.cs
+++ FuelCalculation/FuelCalculation/IFuelCalc.cs
@@ -2,8 +2,22 @@
 
 namespace FuelCalculation
 {
-    public interface IFuelCalc
+    public interface ITransportProperty
     {
-
+        /// <summary>
+        /// Метод расчета расхода топлива транспортным средством
+        /// </summary>
+        /// <returns></returns>
+        bool SuccessProbobility();
+        double GetWearRate();
+        double GetFuelWaste();
+        double GetSpeed();
+        double GetWay();
+        double GetTankVolume();
+        double SetWearRate();
+        double SetFuelWaste();
+        double SetSpeed();
+        double SetWay();
+        double SetTankVolume();
     }
 }
