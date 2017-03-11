2f5377a17a7bcd11ba3eb636766e1a115faffaa6
timafayl <timafayl@gmail.com>
2017-03-11 22:02:18 +0700

    +binary seialization

---
+++ FuelCalculation/TravelingSuccessCalculationView/FilePath.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelingSuccessCalculationView
+{
+    /// <summary>
+    /// Класс, хранящий имя файла, в который будет производиться сериализация.
+    /// </summary>
+    class FilePath
+    {
+        public static string filepath = "transport.tnp";
+    }
+}
--- FuelCalculation/TravelingSuccessCalculationView/Serialization.cs
+++ FuelCalculation/TravelingSuccessCalculationView/Serialization.cs
@@ -1,39 +1,43 @@
 ﻿using FuelCalculation;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TravelingSuccessCalculationView
 {
     public class Serialization
     {
-        public static void Deserialize(ref List<ITransportProperty> myList)
-        {
-            string filename = FileName.SettingsFile;
-            if (File.Exists(filename))
-            {
-                using (FileStream stream = new FileStream(filename, FileMode.Open))
-                {
-                    XmlSerializer xser = new XmlSerializer(typeof(List<ITransportProperty>));
-                    myList = (List<ITransportProperty>)xser.Deserialize(stream);
-                    stream.Close();
-                }
-            }
-            else myList = null;
-        }
-
+        /// <summary>
+        /// Сериализует получаемый на вход список объектов.
+        /// </summary>
+        /// <param name="myList">Список объектов, который будет сериализован</param>
         public static void Serialize(List<ITransportProperty> myList)
         {
-            string filename = FileName.SettingsFile;
+            string filename = FilePath.filepath;
 
             if (File.Exists(filename)) File.Delete(filename);
 
-            using (FileStream stream = new FileStream(filename, FileMode.Create))
+            var formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
             {
-                XmlSerializer xser = new XmlSerializer(typeof(List<ITransportProperty>));
-                xser.Serialize(stream, myList);
-                stream.Close();
+                formatter.Serialize(fs, myList);
+            }
+        }
+
+        /// <summary>
+        /// Десериализует данные в получаемый на вход список.
+        /// </summary>
+        /// <param name="myList">Список, в который будут десериализованы данные из файла</param>
+        public static void Deserialize(ref List<ITransportProperty> myList)
+        {
+            string filename = FilePath.filepath;
+
+            var formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                myList = (List<ITransportProperty>)formatter.Deserialize(fs);
             }
         }
     }
 }
+
