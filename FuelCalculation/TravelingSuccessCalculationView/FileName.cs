e33c6f772f01e60da6d0d506af691a05636ee201
timafayl <timafayl@gmail.com>
2017-03-11 14:06:27 +0700

    +class for serialization (class Settings)

---
+++ FuelCalculation/TravelingSuccessCalculationView/FileName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSuccessCalculationView
+{
+    class FileName
+    {
+        public static string SettingsFile = "settings.xml";
+    }
+}
---
+++ FuelCalculation/TravelingSuccessCalculationView/Settings.cs
@@ -0,0 +1,42 @@
+﻿using FuelCalculation;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TravelingSuccessCalculationView
+{
+    public class Settings
+    {
+        public static Settings GetSettings()
+        {
+            Settings settings = null;
+            string filename = FileName.SettingsFile;
+            if (File.Exists(filename))
+            {
+                using (FileStream stream = new FileStream(filename, FileMode.Open))
+                {
+                    XmlSerializer xser = new XmlSerializer(typeof(List<ITransportProperty>));
+                    settings = (Settings)xser.Deserialize(stream);
+                    stream.Close();
+                }
+            }
+            else settings = new Settings();
+
+            return settings;
+        }
+
+        public void Save()
+        {
+            string filename = FileName.SettingsFile;
+
+            if (File.Exists(filename)) File.Delete(filename);
+
+            using (FileStream stream = new FileStream(filename, FileMode.Create))
+            {
+                XmlSerializer xser = new XmlSerializer(typeof(List<ITransportProperty>));
+                xser.Serialize(stream, this);
+                stream.Close();
+            }
+        }
+    }
+}
